7a3dac29c1879025054522078f7ad23d3c7ae077
Vinicius Molon <molon.vinicius@gmail.com>
2020-04-25 16:55:59 -0300

    organizei em uma pasta a function calculadora

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,9 @@
+var soma = require("./somar.js");
+var sub = require("./subtrair.js");
+var mult = require("./multiplicar.js");
+var div = require("./dividir.js");
+
+console.log(soma(4,5));
+console.log(sub(4,5));
+console.log(mult(4,5));
+console.log(div(4,5));
--- calculadora.js
+++
@@ -1,9 +0,0 @@
-var soma = require("./somar.js");
-var sub = require("./subtrair.js");
-var mult = require("./multiplicar.js");
-var div = require("./dividir.js");
-
-console.log(soma(4,5));
-console.log(sub(4,5));
-console.log(mult(4,5));
-console.log(div(4,5));
