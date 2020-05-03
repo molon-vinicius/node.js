58c953684f5725d918d3e1c290f2b79549d1bd0b
Vinicius Molon <molon.vinicius@gmail.com>
2020-05-03 12:35:58 -0300

    criando tabelas e inserindo dados via sequelize

--- http/conexao.js
+++ http/conexao.js
@@ -4,9 +4,56 @@ const sequelize = new Sequelize('CADASTRO','root','Muspelheim@00',{
 	dialect:'mysql'
 })
 
-sequelize.authenticate().then(function(){
-	console.log("Conectado com sucesso.")	
-}).catch(function(erro){
-	console.log("Falha ao tentar conectar: " + erro)
+// TESTAR A CONEXÃO DO BANCO
+// sequelize.authenticate().then(function(){
+//	console.log("Conectado com sucesso.")	
+// }).catch(function(erro){
+//	console.log("Falha ao tentar conectar: " + erro)
+// })
+
+/*
+const Postagem = sequelize.define('POSTAGENS',{
+	  titulo:{
+	  	type: Sequelize.STRING
+
+	  },
+	  conteudo:{
+	  	type: Sequelize.TEXT
+	  }
 })
 
+//Postagem.sync({force:true})
+
+Postagem.create({
+	titulo: "Primeiro Titulo",
+	conteudo: "Primeiro Conteudo"
+})
+
+//CREATE TABLE, {COLUMNS} 
+const Usuario = sequelize.define('USUARIOS',{
+	nome: {
+		type: Sequelize.STRING
+	},
+	sobrenome: {
+		type: Sequelize.STRING
+	},
+	idade: {
+		type: Sequelize.INTEGER
+	},
+	email: {
+		type: Sequelize.STRING
+	}
+})
+
+// FORÇA A CRIAÇÃO DA TABELA
+//Usuario.sync({force:true}) 
+
+//INSERT INTO 
+Usuario.create({
+	nome: "Vinicius",
+	sobrenome: "Freitas",
+	idade: 30,
+	email: "vinicius@email.com"
+})
+
+*/
