f71a88ad1bb4b6add846842cd68483bfdc765d8a
Vinicius Molon <molon.vinicius@gmail.com>
2020-05-10 18:32:11 -0300

    integração com banco de dados

--- handlebar.js
+++ handlebar.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
-const bodyParser = require('body-parser')
-const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
+const Post = require("./models/Posts");
+const Sequelize = require("sequelize");
 
 //Config
   // Template Engine
@@ -22,9 +23,31 @@ const Sequelize = require('sequelize');
   	  res.render(__dirname +'/views/layouts/formulario');
   })
 
-  app.post('/add', function(req,res){
-  	  res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo);
+  app.get('/',function(req,res){
+        Post.findAll({order: [['id','DESC']]}).then(function(posts){
+      res.render(__dirname +'/views/layouts/home',{posts: posts})
+      })//.catch(erro)
   })
+  app.post('/add', function(req,res){
+    Post.create({
+      titulo: req.body.titulo,
+      conteudo: req.body.conteudo
+    }).then(function(){
+     // res.send("Post enviado com sucesso.")
+     res.redirect('/')
+    }).catch(function(erro){
+      res.send("Post nao enviado. Erro: " + erro)
+    })
+  	//  res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo);
+  })
+    // Deletar
+  app.get('/deletar/:id',function(req,res){
+    Post.destroy({where: {'id':req.params.id}}).then(function(){
+      res.send('Deletado com sucesso.')
+    }).catch(function(erro){
+      res.send('Deu erro amiguinho, não existe.')
+    })
+})
 
 //Tem que ser sempre o último evento do arquivo
 app.listen(8081, function(){
